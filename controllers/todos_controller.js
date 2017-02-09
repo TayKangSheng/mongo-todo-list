6c277a5993f84d5c356765312e10090f94583039
Kang Sheng Tay <kangsheng@gmail.com>
2017-02-09 17:13:11 +0800

    completed assignment

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,20 +1,108 @@
 // TODO. import TODO Model ;-)
-// const Todo = require('../models/todo')
+const Todo = require('../models/todo')
 
 function create (params) {
   // create a new TODO and console log the response
+  const name = params.name
+  const description = params.description
+  const completed = params.completed
+
+  if (!name) {
+    console.log('Please input a valid name')
+  }
+  if (name.length < 5) {
+    console.log('Name should be more than 5 characters')
+  }
+
+  Todo.create(params, function (err, saved) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('New todo created!\n' + saved)
+  })
 }
 function list () {
   // console log the list of all TODOs
+  Todo.find({}, function (err, outputs) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log(outputs)
+  })
 }
 function show (id) {
   // find the TODO with this id and console log it
+  Todo.find({ _id: id }, function (err, output) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    // assuming id is unique
+    console.log(output[0])
+  })
 }
 function update (id, params) {
   // find the TODO with this id and update it's params. console log the result.
+  const name = params.name
+  const description = params.description
+  const completed = params.completed
+
+  if (!id) {
+    console.log('Please provide an ID')
+    return
+  }
+  if (name) {
+    Todo.update({_id: id}, { $set: { name: params.name } }, function (err, output) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      // console.log('Updated name')
+    })
+  }
+  if (description) {
+    Todo.update({_id: id}, { $set: { description: params.description } }, function (err, output) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      // console.log('Updated description')
+    })
+  }
+  if (completed) {
+    Todo.update({_id: id}, { $set: { description: params.completed } }, function (err, output) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      // console.log('Updated completed')
+    })
+  }
+
+  console.log('Update completed')
 }
 function destroy (id) {
   // find the TODO with this id and destroy it. console log success/failure.
+
+  Todo.remove({ _id: id }, function (err, output) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('Todo removed')
+  })
+}
+function destroyAll () {
+  // console.log('Are you sure?')
+  Todo.remove({}, function (err, output) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('All todos destroyed.')
+  })
 }
 
 module.exports = {
@@ -22,5 +110,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- models/todo.js
+++ models/todo.js
@@ -1,1 +1,17 @@
 // Mongoose Schema and Models goes here
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/mongo-todo-list')
+mongoose.Promise = global.Promise
+
+const todoSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  completed: String
+}, {
+  versionKey: false // You should be aware of the outcome after set to false
+})
+
+const TODO = mongoose.model('Todo', todoSchema)
+
+module.exports = TODO
